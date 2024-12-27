8620fee48d94bdee2e7fa50d619f6af8f6e25af3
Giacomo Wilson <geicomoservices@gmail.com>
2024-12-27 12:03:03 -0800

    clean up

--- test/programs/90sassets/templates/topbar.php
+++
@@ -1,8 +0,0 @@
-<div class="logo">
-	<a>Welcome To Oldschool Geicomo.com</a>
-</div>	
-<div class="topbar">
-	<a href="https://geicomo.com/90s/index"><button>Home</button></a>
-	<a href="https://geicomo.com/90s/about"><button>About</button></a>
-	<a href="https://geicomo.com/90s/links"><button>Links</button></a>
-</div>
